942a208a4b60255df7f80409cd779ffddd9f93b0
dxtsfdabw <151771981+dxtsfdabw@users.noreply.github.com>
2023-11-22 13:33:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("My Parent's New Healthy Diner")
 st.header("  Breakfast Menu")
-st.subheader(\t"\tOmega 3 & Blueberry Oatmeal")
+st.subheader("\tOmega 3 & Blueberry Oatmeal")
 st.subheader("    Kale, Spinach & Rocket Smoothie")
 st.subheader("    Hard-Boiled Free-Range Egg")
