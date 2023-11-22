83493a6a0a2b9fa7c000c9892b60ddcc4f9fb8b9
dxtsfdabw <151771981+dxtsfdabw@users.noreply.github.com>
2023-11-22 13:27:58 -0500

    Update streamlit_app.py
    Changed two line to use subheader instead of text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("My Parent's New Healthy Diner")
 st.header("Breakfast Menu")
-st.text("Omega 3 & Blueberry Oatmeal")
-st.text("Kale, Spinach & Rocket Smoothie")
+st.subheader("Omega 3 & Blueberry Oatmeal")
+st.subheader("Kale, Spinach & Rocket Smoothie")
 st.text("Hard-Boiled Free-Range Egg")
