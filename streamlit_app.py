c99c8d30aba83977a9c655f25b141655a0e9bd7a
dxtsfdabw <151771981+dxtsfdabw@users.noreply.github.com>
2023-11-22 13:30:08 -0500

    Update streamlit_app.py
    added tabs for formatting 
    change remain text function to use subheader

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("My Parent's New Healthy Diner")
-st.header("Breakfast Menu")
-st.subheader("Omega 3 & Blueberry Oatmeal")
-st.subheader("Kale, Spinach & Rocket Smoothie")
-st.text("Hard-Boiled Free-Range Egg")
+st.header("  Breakfast Menu")
+st.subheader("    Omega 3 & Blueberry Oatmeal")
+st.subheader("    Kale, Spinach & Rocket Smoothie")
+st.subheader("    Hard-Boiled Free-Range Egg")
