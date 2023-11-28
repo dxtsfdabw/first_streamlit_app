d2fdacbef2de4edea17470d1c524374a69fef29c
dxtsfdabw <151771981+dxtsfdabw@users.noreply.github.com>
2023-11-28 16:56:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,21 +62,23 @@ if streamlit.button('Get Fruit Load List'):
    streamlit.header("The fruit load list contains ")
    streamlit.dataframe(my_data_row)
 
-
 # Don't run anything past here while we troubleshoot
-streamlit.stop()
+# streamlit.stop()
 
 # Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+       sqlstmt = "insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('" + new_fruit + "')"
+       # streamlit.write(sqlstmt)     
+       my_cur.execute(sqlstmt)
+       return "Thanks for adding " + new_fruit
+        
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-sqlstmt = "insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('" + add_my_fruit + "')"
-streamlit.write(sqlstmt)
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute(sqlstmt)
-streamlit.write("Thanks for adding " , add_my_fruit)
+if streamlit.button('Add a Fruit to the list'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   back_from_function = insert_row_snowflake(add_my_fruit)
+   streamlit.text(back_from_function)
 
-#This will not work correctly, but just go with it for now
-sqlstmt = "insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')"
-my_cur.execute(sqlstmt);
+
 
 
